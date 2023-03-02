895749847456e66d14d1e001a9489b7996c8f516
ismahaneyy <liciousmayrah@gmail.com>
2023-03-02 20:24:17 +0300

    defined a function allpets that it calls the useStore hook with an argument of petsKeeper, which is a reference to a store or state management system

--- src/views/Allpets.js
+++ src/views/Allpets.js
@@ -1,2 +1,12 @@
 import { useStore } from "zustand";
 import { petsKeeper } from "../data/PetsStore";
+
+function AllPets(){
+    const pets = useStore(petsKeeper)
+    console.log(pets.petsList)
+    return (<>
+
+    </>)
+}
+
+export default AllPets;
