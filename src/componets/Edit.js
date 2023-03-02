d038236a8dbc9d58fb0e741b3fa457aabdcfb9a8
ismahaneyy <liciousmayrah@gmail.com>
2023-03-02 21:01:03 +0300

    If showForm is false, it returns a button with the text 'Update pet'. When the button is clicked, it sets the showForm state to true and renders the form

--- src/componets/Edit.js
+++ src/componets/Edit.js
@@ -26,5 +26,24 @@ function Edit({pet}){
     console.log(r.status)
     );
     }
-    
+    if(showForm === false){
+        return <button 
+        onClick={() => setShowForm(true)
+        }>Update pet</button>
+    }else{
+        return <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Pet name" onChange={(e) =>{
+        setEditPet({...editPet, "name":e.target.value})
+        }}></input>
+        <input type="text" placeholder="Pet breed" onChange={(e) =>{
+        setEditPet({...editPet, "breed" : e.target.value})
+        }}></input>
+        <input type="url" placeholder="Pet image url" onChange={(e) =>{
+        setEditPet({...editPet, "image_url" : e.target.value})
+        }}></input>
+        <button type="submit">Update</button>
+        </form>
+    }
 }
+
+export default Edit;
