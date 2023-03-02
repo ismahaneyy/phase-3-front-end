d188e28e36d152185133523518c45d1150e045d2
ismahaneyy <liciousmayrah@gmail.com>
2023-03-02 20:34:05 +0300

    defined a function mypets that uses useEffect hook to get the pets and returns them

--- src/views/Mypets.js
+++ src/views/Mypets.js
@@ -2,3 +2,23 @@ import { useStore } from "zustand";
 import { petsKeeper } from "../data/PetsStore";
 import axios from "axios";
 import { useState,useEffect } from "react";
+
+function Mypets(){
+    const myPets = useStore(petsKeeper);
+    const [pets, setPets] = useState([]);
+    useEffect(() => {
+        //this will be the fetch to a specific users pets
+        axios.get("https://philoxenia.onrender.com/pets").then((r) => setPets(r.data));
+            }, []);
+            useEffect(() => {
+              myPets.setpetsKeeper(pets);
+            }, [pets]);
+    return(
+        <>
+
+        </>
+)
+
+}
+
+export default Mypets
