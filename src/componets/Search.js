da420f3fb5319fe7c160964126181984bc88112a
ismahaneyy <liciousmayrah@gmail.com>
2023-03-02 21:12:20 +0300

    defined a function Search, which renders an input field with a placeholder text 'Search pet name or breed'. When a user types into the input field, the handleSearch function is called, which sends an HTTP POST request to the URL and returns the specific pet searched

--- src/componets/Search.js
+++ src/componets/Search.js
@@ -1,3 +1,19 @@
 import axios from "axios";
 import { useStore } from "zustand";
 import { petsKeeper } from "../data/PetsStore";
+
+function Search(){
+    const pets = useStore(petsKeeper)
+    const handleSearch = (e) => {
+    axios.post(`https://philoxenia.onrender.com/pets/search`,{
+        query: e.target.value
+    }).then( (r) =>
+        pets.setpetsKeeper(r.data)
+    )
+    } 
+    return(
+    <input type="text" placeholder="Search pet name or breed" onChange={handleSearch}></input>
+    )
+    }
+export default Search;
+    
