c5ddd97926f9e3ef5650221b1d38541675ab68c9
ismahaneyy <liciousmayrah@gmail.com>
2023-03-02 20:39:51 +0300

    it defines a function called AddPet which handles the creation of a new pet object and sends a POST request to a remote server using the Axios library to add the new pet to a pets database

--- src/componets/AddPets.js
+++ src/componets/AddPets.js
@@ -2,3 +2,24 @@ import { useState } from "react"
 import axios from "axios"
 import { useStore } from "zustand"
 import { petsKeeper } from "../data/PetsStore"
+
+function AddPet(){
+    const pets = useStore(petsKeeper)
+    const [display,setDisplay] = useState(false)
+    const [newPet, setNewPet] = useState({
+    "name":"",
+    "breed": "", 
+    "image": ""
+    })
+    //console.log(newPet)
+    const handleSubmit = (e)  =>{
+    e.preventDefault()
+    setDisplay(false)
+    axios.post(`https://philoxenia.onrender.com/pets/create`,newPet).then((r) =>
+    r.status === 201
+    ? axios
+        .get("https://philoxenia.onrender.com/pets")
+        .then((r) => pets.setpetsKeeper(r.data))
+    : console.log(r.status)
+);
+    }}
