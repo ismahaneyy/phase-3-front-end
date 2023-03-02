508b271053b9093f70f03b2e5eac478e1175076c
ismahaneyy <liciousmayrah@gmail.com>
2023-03-02 21:18:37 +0300

    added the required imports

--- src/views/Allpets.js
+++ src/views/Allpets.js
@@ -1,5 +1,9 @@
+import PetCard from "../components/Petcard";
 import { useStore } from "zustand";
 import { petsKeeper } from "../data/PetsStore";
+import Search from "../components/Search";
+import AddPet from "../components/AddPet";
+import { Link } from "react-router-dom";
 
 function AllPets(){
     const pets = useStore(petsKeeper)
