31d5ca64a19775ef9d7146f3ee734c3c425f2745
ismahaneyy <liciousmayrah@gmail.com>
2023-03-02 21:15:33 +0300

    defined a function Login which renders a form with two input fields and a submit button. The handleSubmit function is called when the form is submitted

--- src/componets/Login.js
+++ src/componets/Login.js
@@ -1,1 +1,17 @@
 import { useNavigate } from "react-router-dom";
+
+function Login(){
+    const redirect = useNavigate()
+    const handleSubmit = (e) =>{
+     e.preventDefault()
+     redirect("/mypets")
+    }
+    return(
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="username"></input>
+            <input type="password" placeholder="password"></input>
+            <button>Login</button>
+        </form>
+    )
+    }
+export default Login;
