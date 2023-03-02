e14fe63a3ec8e52d097ca8541c3055a8b0ebd798
ismahaneyy <liciousmayrah@gmail.com>
2023-03-02 20:59:03 +0300

    defined a function edit that declares state as false at first then uses axios to get and patch a pet when a user wants to update the pet

--- src/componets/Edit.js
+++ src/componets/Edit.js
@@ -2,3 +2,29 @@ import { useStore } from "zustand";
 import { petsKeeper } from "../data/PetsStore";
 import { useState } from "react";
 import axios from "axios";
+
+function Edit({pet}){
+    const pets = useStore(petsKeeper)
+    const [showForm,setShowForm] = useState(false)
+    const [editPet, setEditPet] = useState({
+    "name" : "",
+    "breed" : "",
+    "image_url" : ""
+    })
+    console.log(editPet)
+    const handleSubmit = (e)  =>{
+    e.preventDefault()
+    setShowForm(false)
+    axios
+    .patch(`https://philoxenia.onrender.com/update/pets`)
+    .then((r) =>
+      r.status === 200
+        ? axios
+            .get("https://philoxenia.onrender.com/pets")
+            .then((r) => pets.setpetsKeeper(r.data))
+        : 
+    console.log(r.status)
+    );
+    }
+    
+}
