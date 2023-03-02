bd250b49aef0e0c650d16c33c3425f0f3253af18
ismahaneyy <liciousmayrah@gmail.com>
2023-03-02 20:18:07 +0300

    defines a constant thePetsData which takes a set function as input and returns an object with two properties petslist and pets keeper that takes a pet object as input and calls the set function provided to thePetsData, updating the petsList property with the pet object

--- src/data/PetsStore.js
+++ src/data/PetsStore.js
@@ -1,2 +1,13 @@
 import { create } from "zustand";
 
+const thePetsData = (set) =>({
+    petsList:[],
+    setpetsKeeper:(pet) =>{
+        set((state) =>({
+            petsList:pet
+        })) 
+    }
+    })
+    const petsKeeper = create(thePetsData)
+    
+export {petsKeeper};
