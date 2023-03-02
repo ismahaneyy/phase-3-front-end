a521cd6b287d7c2f2fd1903d3fac5677143d72a2
ismahaneyy <liciousmayrah@gmail.com>
2023-03-02 20:45:11 +0300

    defined a function Delete, which allows the user to delete a specific pet from a remote server using the Axios library

--- src/componets/Delete.js
+++ src/componets/Delete.js
@@ -1,3 +1,19 @@
 import axios from "axios";
 import { useStore } from "zustand";
 import { petsKeeper } from "../data/PetsStore";
+
+function Delete({pet}) {
+    const pets = useStore(petsKeeper);
+    const handleDelete = () => {
+      axios
+        .delete(`https://philoxenia.onrender.com/delete/pets/${pet.id}`)
+        .then((r) =>
+          r.status === 200
+            ? axios
+                .get("https://philoxenia.onrender.com/pets")
+                .then((r) => pets.setpetsKeeper(r.data))
+            : console.log(r.status)
+        );
+    };
+    return <button onClick={handleDelete}> Delete</button>;
+  }
